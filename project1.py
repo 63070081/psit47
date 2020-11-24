c8164ee71c9a4ef2947470fce11a9a51ac3358a1
MaxThanit <73686016+63070081@users.noreply.github.com>
2020-11-24 15:29:43 +0700

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,10 +1,10 @@
 """ Booking Tickets """
 def main():
     """ Welcome stage before start"""
-    welcome1 = str(input("Welcome to Movie Booking Tickets system Press 'Y' to start or Press 'N' to cancel...."))
+    welcome1 = str(input("Welcome to Movie Booking Tickets system [Press 'Y' to start or Press 'N' to cancel.] Ans:"))
     if welcome1 == 'Y' or welcome1 == 'y':
         print("Okay")
-        func1('Alive', 'Frozen 2', 'Iron Mask')
+        func1('1.Alive', '2.Frozen 2', '3.Iron Mask')
     elif welcome1 == 'N' or welcome1 == 'n':
         print("Thank you for using. See you next time ! ;D")
     else:
@@ -16,5 +16,6 @@ def func1(mov1,mov2,mov3):
     print("Today we have a movies for you !")
     print("Here a list of movies we have today :D")
     print(mov1, mov2, mov3, sep="\n")
+    num1 = int(input("Which one do you like? [Press 1, 2, 3 to select]  Ans:"))
 
 main()
