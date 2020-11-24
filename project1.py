7ef29d78bdd692f901c8f4400f2b24f6af85302c
MaxThanit <73686016+63070081@users.noreply.github.com>
2020-11-24 15:05:23 +0700

    Create project1.py

---
+++ project1.py
@@ -0,0 +1,17 @@
+""" Booking Tickets """
+def main():
+    """ Welcome stage before start"""
+    welcome1 = str(input("Welcome to Movie Booking Tickets system Press 'Y' to start or Press 'N' to cancel...."))
+    if welcome1 == 'Y' or 'y':
+        print("Okay")
+        func1('Alive', 'Frozen 2', 'Iron Mask')
+    elif welcome1 == 'N' or 'n':
+        print("Thank you for using. See you next time ! ;D")
+    
+def func1(mov1,mov2,mov3):
+    """ Let's get started """
+    print("Today we have a movies for you !")
+    print("Here a list of movies we have today :D")
+    print(mov1, mov2, mov3, sep="\n")
+
+main()
