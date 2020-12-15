788d6abb7560bda1e21e4d7e346a651bb1c6e333
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2020-12-15 10:59:28 +0700

    Update OtherWindow3.py

--- OtherWindow3.py
+++ OtherWindow3.py
@@ -19,28 +19,36 @@ class Ui_OtherWindow3(object):
         self.window.show()
     def setupUi(self, OtherWindow3):
         OtherWindow3.setObjectName("OtherWindow3")
-        OtherWindow3.resize(712, 294)
+        OtherWindow3.resize(712, 296)
         self.centralwidget = QtWidgets.QWidget(OtherWindow3)
         self.centralwidget.setObjectName("centralwidget")
-
         self.S17 = QtWidgets.QPushButton(self.centralwidget)
         self.S17.setGeometry(QtCore.QRect(40, 170, 291, 51))
         self.S17.setObjectName("S17")
         self.S17.clicked.connect(self.openseat)
 
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(320, 120, 71, 20))
+        self.label.setGeometry(QtCore.QRect(280, 120, 151, 41))
         self.label.setObjectName("label")
-
-        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(200, 20, 301, 71))
-        self.textBrowser.setObjectName("textBrowser")
-
         self.S19 = QtWidgets.QPushButton(self.centralwidget)
         self.S19.setGeometry(QtCore.QRect(380, 170, 291, 51))
         self.S19.setObjectName("S19")
         self.S19.clicked.connect(self.openseat)
 
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(0, 0, 711, 261))
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("soul.jpg"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(220, 20, 251, 71))
+        self.label_3.setObjectName("label_3")
+        self.label_2.raise_()
+        self.S17.raise_()
+        self.label.raise_()
+        self.S19.raise_()
+        self.label_3.raise_()
         OtherWindow3.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(OtherWindow3)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 712, 26))
@@ -57,13 +65,9 @@ class Ui_OtherWindow3(object):
         _translate = QtCore.QCoreApplication.translate
         OtherWindow3.setWindowTitle(_translate("OtherWindow3", "MainWindow"))
         self.S17.setText(_translate("OtherWindow3", "17.30"))
-        self.label.setText(_translate("OtherWindow3", "13/12/2020"))
-        self.textBrowser.setHtml(_translate("OtherWindow3", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:28pt;\">Soul</span></p></body></html>"))
+        self.label.setText(_translate("OtherWindow3", "<html><head/><body><p><span style=\" font-size:16pt; color:#ffffff;\">16/12/2020</span></p></body></html>"))
         self.S19.setText(_translate("OtherWindow3", "19.30"))
+        self.label_3.setText(_translate("OtherWindow3", "<html><head/><body><p align=\"center\"><span style=\" font-size:36pt; font-weight:600; color:#ffffff;\">Soul</span></p></body></html>"))
 
 
 if __name__ == "__main__":
